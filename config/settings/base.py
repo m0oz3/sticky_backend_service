41be2eb6917e89a62d8b6f027586c2e3223d9881
Moussa Syoufi <m0z3@protonmail.com>
2024-08-10 17:38:14 +0300

    added django channels

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,6 +60,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # APPS
 # ------------------------------------------------------------------------------
 DJANGO_APPS = [
+    "daphne",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -86,6 +87,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "sticky_backend_service.users",
+    "sticky_backend_service.game",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
