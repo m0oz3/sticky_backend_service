9b2cc6bde88ef8bd87829f7079f905af0281623b
Moussa Syoufi <m0z3@protonmail.com>
2024-08-12 07:20:13 +0300

    stuff

--- sticky_backend_service/game/consumers.py
+++ sticky_backend_service/game/consumers.py
@@ -1,26 +1,34 @@
 import json
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django_celery_beat.models import PeriodicTask
 
 
 class GameConsumer(AsyncWebsocketConsumer):
+    state = {"player1": {}, "player2": {}}
+
     async def connect(self):
         await self.channel_layer.group_add("game_room", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("game_room", self.channel_name)
+        things = await database_sync_to_async(PeriodicTask.objects.filter)(
+            name="Ticker for game",
+            task="game_tick",
+        )
+        await database_sync_to_async(things.delete())()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        if text_data_json["type"] == "state":
+            text_data_json = text_data_json["data"]
+            self.state.update(text_data_json)
+            await self.channel_layer.group_send(
+                "game_room",
+                {"type": "state_update", "data": self.state},
+            )
 
-        await self.channel_layer.group_send(
-            "game_room",
-            {"type": "chat.message", "message": message},
-        )
-
-    async def chat_message(self, event):
-        message = event["message"]
-
-        await self.send(text_data=json.dumps({"message": message}))
+    async def state_update(self, text_data):
+        await self.send(json.dumps({"type": "state", "data": text_data["data"]}))
